1ab3568df0200dc73a19b1f695e2a4a15dc2c95a
quandarealest <buikhacminhquan@hotmail.com>
2022-03-14 00:44:09 -0400

    rename the db connection

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect("localhost","root","","project1-bookexchange");
+$connection = mysqli_connect("localhost","root","","book_exchange");
 if(mysqli_connect_errno()){
   echo "error occured while connecting to Database: ".mysqli_connect_errno();
 }
