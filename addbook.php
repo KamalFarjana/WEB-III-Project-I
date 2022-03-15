6c4ec1153a784db297dcf1d66a2abeac47234056
Jenish Rank <77623980+jrank1@users.noreply.github.com>
2022-03-15 15:03:14 -0400

    sql and add book page added

--- addbook.php
+++ addbook.php
@@ -1,3 +1,75 @@
+<?php
+include("connect.php");
+include("function.php");
+$error="";
+$success_msg="";
+
+if(isset($_POST['AddBook'])){
+  #variable_initialization
+  $BookTitle=$_POST['BookTitle'] ?? "";
+  $BookAuthor=$_POST['BookAuthor'] ?? "";
+  $BookISBN=$_POST['BookISBN'] ?? "";
+  $BookGenre=$_POST['BookGenre'] ?? "";
+  $BookCondition=$_POST['BookCondition'] ?? "";
+  $PickupPhone=$_POST['PickupPhone'] ?? "";
+  $PickupEmail=$_POST['PickupEmail'] ?? "";
+  $PickupNote=$_POST['PickupNote'] ?? "";
+
+  $image = $_FILES['BookCover']['name'] ?? "";
+  $tmp_image = $_FILES['BookCover']['tmp_name'] ?? "";
+  $imageSize = $_FILES ['BookCover']['size'] ?? "";
+  $allowed_image_ext = 'jpg';
+  $div_image = explode(".", $image); #seperate name, ext
+  $image_ext = end($div_image); #get extention
+  $unique_image_name = substr(md5(time()), 0, 10).".".$image_ext; #generate unique name
+
+  #$SubmittedBy = $_SESSION["username"];
+  $SubmittedBy = '1';
+  $Status = '1';
+
+  $Date = date('m/d/Y h:i:s a', time());
+
+  if(strlen($BookTitle)<1){
+    $error= "Please enter the book title!";
+  }
+  else if (strlen($BookAuthor) < 1){
+    $error = "Please enter the name of the book's author!";
+  }
+  else if (strlen($BookGenre) < 1){
+    $error = "Please enter the genre for the book!";
+  }
+  else if (strlen($BookCondition) < 1){
+    $error = "Please select book condition!";
+  }
+  else if (strlen($PickupPhone) < 1 == 0 && strlen($PickupEmail) < 1){
+    $error = "Please enter either phone number or email for pickup details!";
+  }
+  else if ($imageSize > 1048576){
+    $error = "Image size must be less than 1 MB";
+  }
+  else if (strcmp($image_ext, $allowed_image_ext) !==0){
+    $error = "Only jpg file is allowed!";
+  }
+  else
+  {
+    $insertQuery = "INSERT INTO books (Title, Author, ISBN, Genre, Condition, Book_image, User_id, Status, PhoneNumber, Email, Note, AddedDate)
+                    VALUES ('$BookTitle','$BookAuthor','$BookISBN','$BookGenre','$BookCondition','$unique_image_name','$SubmittedBy','$Status','$PickupPhone','$PickupEmail', '$PickupNote', '$Date')";
+
+    if(mysqli_query($connection, $insertQuery)){
+      if(move_uploaded_file($tmp_image, "cover/$unique_image_name")) {
+        $success_msg = "You have successfully added the book";
+      }
+    }
+    else
+    {
+      $error = "The attempt was unsuccessfull.";
+    }
+  }
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -61,72 +133,65 @@
           </div>
 
           <div class="row">
-            <form class="form-horizontal addbook">
-              <h3>Book Details</h3><hr>
+          <?php
+            if(strlen($error)>0){ ?>
+              <div id="error"> <?php echo $error ?>  </div>
+          <?php } else {?>
+              <div id="success"> <?php echo $success_msg ?></div>
+          <?php } ?>
+          <form method="post" action="addbook.php" enctype="multipart/form-data" class="form-horizontal addbook">
+              <h3>Book Details</h3>
               <table   class="table table-sm profileborder">
                 <tr>
-                  <td><label class="col-sm-7 control-label" style="text-align: left;" for="bookCover">Book Cover</label></td>
+                  <td><label class="col-sm-7 control-label" style="text-align: left;" for="bookCover" name="BookCover">Book Cover</label></td>
                   <td><input type="file" class="form-control" id="bookCover"></td>
                 </tr>
               <tr>  
-                <td><label class="col-sm-7 control-label" style="text-align: left;">Book Title</label></td>
+                <td><label class="col-sm-7 control-label" style="text-align: left;" name="BookTitle">Book Title</label></td>
                 <td><input type="text" class="form-control" placeholder="Enter book title"></td>
               </tr>
               <tr>
-                <td><label class="col-sm-7 control-label" style="text-align: left;">Book's Author</label></td>
+                <td><label class="col-sm-7 control-label" style="text-align: left;" name="BookAuthor">Book's Author</label></td>
                 <td><input type="text" class="form-control" placeholder="Enter book's author"></td>
               </tr>
               <tr>
-                <td><label class="col-sm-7 control-label" style="text-align: left;">Book ISBN</label></td>
+                <td><label class="col-sm-7 control-label" style="text-align: left;" name="BookISBN">Book ISBN</label></td>
                 <td><input type="text" class="form-control" placeholder="Enter ISBN"></td>
               </tr>
                 <tr>
-                <td><label class="col-sm-7 control-label" style="text-align: left;">Book's Genre</label></td>
+                <td><label class="col-sm-7 control-label" style="text-align: left;" name="BookGenre">Book's Genre</label></td>
                 <td><input type="text" class="form-control" placeholder="Enter book's genre"></td>
               </tr>
                 <tr>
-                <td><label class="col-sm-7 control-label" for="BookCondition" style="text-align: left;">Book Condition</label></td>
+                <td><label class="col-sm-7 control-label" for="BookCondition" style="text-align: left;" name="BookCondition">Book Condition</label></td>
                 <td>
                   <select class="form-control" id="BookCondition">
-                    <option>New</option>
-                    <option>Decent</option>
-                    <option>Not so great</option>
+                    <option value="New">New</option>
+                    <option value="Decent">Decent</option>
+                    <option value="NotGreat">Not so great</option>
                   </select>
                 </td>
               </tr>
             </table>
-            </form>
-            <br>
-            <form class="form-horizontal addbook">
-              <h3>Pickup Details</h3><hr>
-              <table   class="table table-sm profileborder">
+              <h3>Pickup Details</h3>
+              <table class="table table-sm profileborder">
                 <tr>
-                  <td><label class="col-sm-7 control-label" style="text-align: left;" for="bookCover">Phone</label></td>
+                  <td><label class="col-sm-7 control-label" style="text-align: left;" name="PickupPhone">Phone No</label></td>
                   <td><input type="text" class="form-control" id="bookCover" placeholder="Enter your phone number"></td>
                 </tr>
               <tr>  
-                <td><label class="col-sm-7 control-label" style="text-align: left;">Email</label></td>
+                <td><label class="col-sm-7 control-label" style="text-align: left;" name="PickupEmail">Email</label></td>
                 <td><input type="email" class="form-control" placeholder="Enter your email address"></td>
               </tr>
               <tr>
-                <td><label class="col-sm-7 control-label" style="text-align: left;" for="pickupNote">Note</label></td>
+                <td><label class="col-sm-7 control-label" style="text-align: left;" for="pickupNote" name="PickupNote">Note</label></td>
                 <td><textarea class="form-control" id="pickupNote" rows="3" placeholder="Add note here"></textarea></td>
               </tr>
-              <tr>
-                <td><label class="col-sm-7 control-label" for="shareInfo" style="text-align: left;">Share</label></td>
-                <td>
-                  <select class="form-control" id="shareInfo">
-                    <option>Everyone</option>
-                    <option>Registred User</option>
-                  </select>
-                </td>
-              </tr>
             </table>
-            </form>
-            <br>
-            <a href="confirm.html"><img src="Images/submit.png" alt="submitButton" style="width:200px;height:60px;"></a>
 
-          </div>
+            <input type="submit" name="AddBook" style="background: url('Images/submit.png'); border:none; background-repeat:no-repeat; width:200px;height:50px;" value="Add Book" />
+            </form>
+          </div>  
 
       
         </div>
