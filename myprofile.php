8731883160d12fea3c27a40011e131d0e2655b59
gauri asok <gasokkum>
2022-03-20 01:05:19 -0400

    Modified editprofile and myprofile

--- myprofile.php
+++ myprofile.php
@@ -1,18 +1,20 @@
 <?php
  include('session.php');
  include('connect.php');
+ function logged_in(){
+   if(isset($_SESSION['email'])){
+     return true;
+   }
+   else{
+     return false;
+   }
+ }
 ?>
 <?php
 $sql="SELECT * FROM registered_user WHERE Email='$user_check'";
 $result=mysqli_query($connection,$sql);
-$bookgiven="SELECT count(Book_id) FROM books where Email='$user_check'";
-$user_id=$row['User_id'];
-$bookborrow="SELECT count(Book_id) FROM borrowed_book where User_id='$user_id'";
-$b=mysqli_query($connection,$bookgiven);
-$bborrow=mysqli_query($connection,$bookborrow);
-$bg=mysqli_fetch_array($b);
-$bb=mysqli_fetch_array($bborrow);
 ?>
+<?php if(logged_in()){ ?>
 <?php
 while($rows=mysqli_fetch_array($result)){
    $imageSrc = '/Images/profile' . '/'. $rows['Image'];
@@ -34,26 +36,26 @@ while($rows=mysqli_fetch_array($result)){
 <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
       <div class="container-fluid">
-        <a class="navbar-brand" href="index.html">Book Xchange</a>
+        <a class="navbar-brand" href="index.php">Book Xchange</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav me-auto mb-2 mb-md-0">
             <li class="nav-item">
-              <a class="nav-link" href="index.html">Home</a>
+              <a class="nav-link" href="index.php">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="aboutus.html">About Us</a>
+              <a class="nav-link" href="aboutus.php">About Us</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="browse.html">Browse Books</a>
+              <a class="nav-link" href="browse.php">Browse Books</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="addbook.html">Add Book</a>
+                <a class="nav-link" href="addbook.php">Add Book</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="faq.html">FAQ</a>
+              <a class="nav-link" href="faq.php">FAQ</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="contactus.php">Contact Us</a>
@@ -63,7 +65,7 @@ while($rows=mysqli_fetch_array($result)){
           <ul class="navbar-nav navbar-right">
 
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-bs-toggle="dropdown" aria-expanded="false"> <?php echo $rows['Username']; ?></a>
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-bs-toggle="dropdown" aria-expanded="false"> <?php echo $rows['UserName']; ?></a>
                 <ul class="dropdown-menu" aria-labelledby="dropdown04">
                     <li><a class="dropdown-item active" href="myprofile.php">My Profile</a></li>
                     <li><a class="dropdown-item" href="myinventory.php">My Inventory</a></li>
@@ -91,36 +93,33 @@ while($rows=mysqli_fetch_array($result)){
 
             <tr>
 
-              <td><?php echo "<img src='Images/profile".$rows['Image']."'"?> class="avatar img-circle" alt="avatar" height="150px" width="140px"> </td>
+              <td><?php echo "<img src='Images/profile/".$rows['Image']."'"?> class="avatar img-circle" alt="avatar" height="150px" width="140px"> </td>
 
 <td>
                 <div class="profile-head" style="text-align: left;">
                   <h3>
-                      <?php echo  $rows['First_Name']." ". $rows['Last_Name']?>
+                      <?php echo  $rows['FirstName']." ". $rows['LastName']?>
 
-                  <h6>
-                      Books Given   : <?php echo $bg[0]?><br>
-                      Book Borrowed : <?php echo $bb[0]?>
-                  </h6>
+
                   <br>
                 </div>
               </td>
                 </tr>
               <tr>
                 <td><label class="col-sm-7 control-label" style="text-align: right;">Username</label></td>
-                <td><input type="text" class="form-control" disabled="true" value="<?php echo $rows['Username']; ?>"></td>
+                <td><input type="text" class="form-control" disabled="true" value="<?php echo $rows['UserName']; ?>"></td>
               </tr>
               <tr>
                 <td><label class="col-sm-7 control-label" style="text-align: right;">First name</label></td>
-                <td><input type="text" class="form-control" disabled="true"  value="<?php echo $rows['First_Name']; ?>"</td>
+                <td><input type="text" class="form-control" disabled="true"  value="<?php echo $rows['FirstName']; ?>"</td>
               </tr>
               <tr>
                 <td><label class="col-sm-7 control-label" style="text-align: right;">Last name</label></td>
-                <td><input type="text" class="form-control" disabled="true"  value="<?php echo $rows['Last_Name']; ?>"></td>
+                <td><input type="text" class="form-control" disabled="true"  value="<?php echo $rows['LastName']; ?>"></td>
               </tr>
               <tr>
                 <td><label class="col-sm-7 control-label" style="text-align: right;">Phone</label></td>
-                <td><input type="tel" class="form-control" disabled="true"  value="<?php echo $rows['Phone']; ?>"></td>
+                <td><input type="tel" class="form-control" disabled="true"  value="<?php echo $rows['PhoneNumber']; ?>"></td>
               </tr>
               <tr>
                 <td><label class="col-sm-7 control-label" style="text-align: right;">E-mail address</label></td>
@@ -133,20 +132,21 @@ while($rows=mysqli_fetch_array($result)){
 
               <tr>
                 <td><label class="col-sm-7 control-label" style="text-align: right;">Postal Code</label></td>
-                <td><input type="text" class="form-control" disabled="true"  value="<?php echo $rows['Postal_Code']; ?>"></td>
+                <td><input type="text" class="form-control" disabled="true"  value="<?php echo $rows['PostalCode']; ?>"></td>
               </tr>
           </table>
             </form>
           </div>
-
+      </div>
+        <br>
       <?php }
       ?>
-
-
-
-        </div>
-        <br>
       </main>
+    <?php  } else {
+        echo ("<script LANGUAGE='JavaScript'>window.alert('Login to your account');window.location.href='signin.php';</script>");
+    }
+     ?>
+
 
       <footer class="footer bg-dark mt-auto py-3 bg-light">
         <div class="container">
